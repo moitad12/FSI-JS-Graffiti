d2b292a4cd15d42afb62b718ac97d4b90c2eed28
Moises Tadeo <moisestadeo10@gmail.com>
2022-06-25 11:37:59 -0700

    finished js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+//Gave all images a border radius of 50px
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = "50px"
+}
+
+//aligned dog names to the left
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = "left"
+}
+
+//change footer color
+let footerColor = document.querySelector('.footer')
+footerColor.style.color = 'red'
+
